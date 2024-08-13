4a066bc97444a01954c7cea013b8f4be840cdad0
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-08-13 21:39:13 +0200

    chore: add projects data

---
+++ src/assets/projects.js
@@ -0,0 +1,126 @@
+import PhotoVtg from './images/projects/photo-vtg.png';
+import PhotoMusic from './images/projects/photo-music.png';
+import PhotoFlashcards from './images/projects/photo-flashcards.png';
+import PhotoOgPreview from './images/projects/photo-og-preview.png';
+import PhotoPortfolioV1 from './images/projects/photo-portfolio-v1.png';
+import PhotoPortfolioAdvanced from './images/projects/photo-portfolio-advanced.png';
+
+const PROJECTS = [
+  {
+    id: '1',
+    title: 'og-preview',
+    image: PhotoOgPreview,
+    description:
+      'A npm-package that generates a preview of how all links appear when shared on social media. Simply execute a CLI command.',
+    descriptionLong:
+      'OG-preview is a CLI tool to preview all available Open Graph meta data from the local development server. This allows the developer to check how the links of the website will appear when shared on social media platforms without deploying your project or checking every URL manually. You simply need to execute a single CLI command.',
+    skills: ['Node', 'Express', 'TypeScript'],
+    githubLink: 'https://github.com/marcorosenbaum/og-preview',
+    websiteLink: 'https://www.npmjs.com/package/og-preview',
+    learnings: [
+      'Setting up a CLI tool',
+      'Using Express to serve static files',
+      'Building a Web Scraper',
+      'Publishing and managing a npm package',
+    ],
+  },
+  {
+    id: '2',
+    title: 'VTG-Musli',
+    image: PhotoVtg,
+    description: 'The website of a local car dealer in Germany.',
+    descriptionLong:
+      'VTG-Musli is a local car dealer in northern Germany. The goal was to create a simple and clean website that showcases the services and partners of the company. ',
+    skills: ['JavaScript', 'Vue', 'Tailwind'],
+    githubLink: 'https://github.com/marcorosenbaum/vtg-musli',
+    websiteLink: 'https://vtg-musli.de',
+    learnings: [
+      'Creating a website from scratch',
+      'Fully responsive design',
+      'Deploying a website via FileZilla (File Transfer Protocol)',
+    ],
+  },
+  {
+    id: '3',
+    title: 'Portfolio-V1',
+    image: PhotoPortfolioV1,
+    description: 'The first version of my portfolio website.',
+    descriptionLong:
+      'This is the first version of my personal portfolio website. I focused on creating a clean and simple portfolio that showcases my projects and skills. ',
+    skills: ['JavaScript', 'Vue', 'Tailwind'],
+    githubLink: 'https://github.com/marcorosenbaum/portfolio',
+    websiteLink: 'https://marcorosenbaum-v1.netlify.app/',
+    learnings: [
+      'Creating a personal portfolio',
+      'Building reusable components',
+      'Working with Figma designs',
+    ],
+  },
+  {
+    id: '4',
+    title: 'Flashcards',
+    image: PhotoFlashcards,
+    description:
+      'Flashcards is a web application where you can create flashcards and keep track of your learning progress.',
+    descriptionLong:
+      'Flashcards is a web application where you can create flashcards and keep track of your learning progress. The goal was to create a simple and clean application that helps people to memorize information while getting a visual feedback of their learning progress.',
+    skills: [
+      'Vue',
+      'Tailwind',
+      'Firebase',
+      'Firestore',
+      'Pinia',
+      'Chart.js',
+      'TipTap',
+    ],
+    githubLink: 'https://github.com/marcorosenbaum/flashcards',
+    websiteLink: 'https://marco-flashcards.netlify.app/',
+    learnings: [
+      'Working with Firebase',
+      'User authentication',
+      'Storing data in Firestore',
+      'Creating a responsive design',
+      'Implemanting tools like Chart.js and TipTap',
+      'Using Pinia for state management',
+      'Form validation',
+    ],
+  },
+  {
+    id: '5',
+    title: 'Music Prototype',
+    image: PhotoMusic,
+    description: 'The Prototype of a music player web application.',
+    descriptionLong:
+      "This is the project that I built during the course 'Vue - The complete guide by Zero to Mastery'. It is a music player web application with user authentication. Users can upload their own songs and comment on other songs.",
+    skills: ['Vue', 'Tailwind', 'Firebase', 'Firestore', 'Pinia'],
+    githubLink: 'https://github.com/marcorosenbaum/music-example',
+    websiteLink: '',
+    learnings: [
+      'User authentication',
+      'Storing data in Firebase',
+      'Translating content with vue-i18n',
+      'Testing with Vitest',
+    ],
+  },
+  {
+    id: '6',
+    title: 'Portfolio-Advanced',
+    image: PhotoPortfolioAdvanced,
+    description:
+      'The second Version of my portfolio website. Implemented netlify functions to create costum website analytics in MongoDB.',
+    descriptionLong:
+      'This is the second version of my personal portfolio website. I focused on creating a clean and simple portfolio that showcases my projects and skills. I implemented netlify functions to create costum website analytics, like tracking clicks, on MongoDB.',
+    skills: ['TypeScript', 'React', 'MongoDB', 'Tailwind', 'Netlify'],
+    githubLink: 'https://github.com/marcorosenbaum/portfolio-advanced',
+    websiteLink: 'https://marcorosenbaum.netlify.app/',
+    learnings: [
+      'Working with MongoDB',
+      'Creating netlify functions',
+      'Tracking clicks on the website',
+      'Using React Hooks',
+      'Implementing a dark-mode',
+    ],
+  },
+];
+
+export default PROJECTS;
