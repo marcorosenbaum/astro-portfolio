93e414848268ca153909507d40a155c54669bcc5
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-08-14 11:51:38 +0200

    chore: added optimized images in different sizes for projects

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,15 +1,70 @@
-import PhotoVtg from './images/projects/photo-vtg.png';
-import PhotoMusic from './images/projects/photo-music.png';
-import PhotoFlashcards from './images/projects/photo-flashcards.png';
-import PhotoOgPreview from './images/projects/photo-og-preview.png';
-import PhotoPortfolioV1 from './images/projects/photo-portfolio-v1.png';
-import PhotoPortfolioAdvanced from './images/projects/photo-portfolio-advanced.png';
+import PhotoVtg from './images/projects/vtg/photo-vtg.png';
+import PhotoVtg400w from './images/projects/vtg/photo-vtg-400w.avif';
+import PhotoVtg600w from './images/projects/vtg/photo-vtg-600w.avif';
+import PhotoVtg800w from './images/projects/vtg/photo-vtg-800w.avif';
+import PhotoVtg1000w from './images/projects/vtg/photo-vtg-1000w.avif';
+import PhotoVtg1200w from './images/projects/vtg/photo-vtg-1200w.avif';
+import PhotoVtg1600w from './images/projects/vtg/photo-vtg-1600w.avif';
+import PhotoVtg2000w from './images/projects/vtg/photo-vtg-2000w.avif';
+
+import PhotoMusic from './images/projects/music/photo-music.png';
+import PhotoMusic400w from './images/projects/music/photo-music-400w.avif';
+import PhotoMusic600w from './images/projects/music/photo-music-600w.avif';
+import PhotoMusic800w from './images/projects/music/photo-music-800w.avif';
+import PhotoMusic1000w from './images/projects/music/photo-music-1000w.avif';
+import PhotoMusic1200w from './images/projects/music/photo-music-1200w.avif';
+import PhotoMusic1600w from './images/projects/music/photo-music-1600w.avif';
+import PhotoMusic2000w from './images/projects/music/photo-music-2000w.avif';
+
+import PhotoFlashcards from './images/projects/flashcards/photo-flashcards.png';
+import PhotoFlashcards400w from './images/projects/flashcards/photo-flashcards-400w.avif';
+import PhotoFlashcards600w from './images/projects/flashcards/photo-flashcards-600w.avif';
+import PhotoFlashcards800w from './images/projects/flashcards/photo-flashcards-800w.avif';
+import PhotoFlashcards1000w from './images/projects/flashcards/photo-flashcards-1000w.avif';
+import PhotoFlashcards1200w from './images/projects/flashcards/photo-flashcards-1200w.avif';
+import PhotoFlashcards1600w from './images/projects/flashcards/photo-flashcards-1600w.avif';
+import PhotoFlashcards2000w from './images/projects/flashcards/photo-flashcards-2000w.avif';
+
+import PhotoOgPreview from './images/projects/og-preview/photo-og-preview.png';
+import PhotoOgPreview400w from './images/projects/og-preview/photo-og-preview-400w.avif';
+import PhotoOgPreview600w from './images/projects/og-preview/photo-og-preview-600w.avif';
+import PhotoOgPreview800w from './images/projects/og-preview/photo-og-preview-800w.avif';
+import PhotoOgPreview1000w from './images/projects/og-preview/photo-og-preview-1000w.avif';
+import PhotoOgPreview1200w from './images/projects/og-preview/photo-og-preview-1200w.avif';
+import PhotoOgPreview1600w from './images/projects/og-preview/photo-og-preview-1600w.avif';
+import PhotoOgPreview2000w from './images/projects/og-preview/photo-og-preview-2000w.avif';
+
+import PhotoPortfolioV1 from './images/projects/portfolio-v1/photo-portfolio-v1.png';
+import PhotoPortfolioV1400w from './images/projects/portfolio-v1/photo-portfolio-v1-400w.avif';
+import PhotoPortfolioV1600w from './images/projects/portfolio-v1/photo-portfolio-v1-600w.avif';
+import PhotoPortfolioV1800w from './images/projects/portfolio-v1/photo-portfolio-v1-800w.avif';
+import PhotoPortfolioV11000w from './images/projects/portfolio-v1/photo-portfolio-v1-1000w.avif';
+import PhotoPortfolioV11200w from './images/projects/portfolio-v1/photo-portfolio-v1-1200w.avif';
+import PhotoPortfolioV11600w from './images/projects/portfolio-v1/photo-portfolio-v1-1600w.avif';
+import PhotoPortfolioV12000w from './images/projects/portfolio-v1/photo-portfolio-v1-2000w.avif';
+
+import PhotoPortfolioAdvanced from './images/projects/portfolio-advanced/photo-portfolio-advanced.png';
+import PhotoPortfolioAdvanced400w from './images/projects/portfolio-advanced/photo-portfolio-advanced-400w.avif';
+import PhotoPortfolioAdvanced600w from './images/projects/portfolio-advanced/photo-portfolio-advanced-600w.avif';
+import PhotoPortfolioAdvanced800w from './images/projects/portfolio-advanced/photo-portfolio-advanced-800w.avif';
+import PhotoPortfolioAdvanced1000w from './images/projects/portfolio-advanced/photo-portfolio-advanced-1000w.avif';
+import PhotoPortfolioAdvanced1200w from './images/projects/portfolio-advanced/photo-portfolio-advanced-1200w.avif';
+import PhotoPortfolioAdvanced1600w from './images/projects/portfolio-advanced/photo-portfolio-advanced-1600w.avif';
+import PhotoPortfolioAdvanced2000w from './images/projects/portfolio-advanced/photo-portfolio-advanced-2000w.avif';
 
 const PROJECTS = [
   {
     id: '1',
     title: 'og-preview',
-    image: PhotoOgPreview,
+    image: {
+      width400: PhotoOgPreview400w,
+      width600: PhotoOgPreview600w,
+      width800: PhotoOgPreview800w,
+      width1000: PhotoOgPreview1000w,
+      width1200: PhotoOgPreview1200w,
+      width1600: PhotoOgPreview1600w,
+      width2000: PhotoOgPreview2000w,
+    },
     description:
       'A npm-package that generates a preview of how all links appear when shared on social media. Simply execute a CLI command.',
     descriptionLong:
@@ -27,7 +82,15 @@ const PROJECTS = [
   {
     id: '2',
     title: 'VTG-Musli',
-    image: PhotoVtg,
+    image: {
+      width400: PhotoVtg400w,
+      width600: PhotoVtg600w,
+      width800: PhotoVtg800w,
+      width1000: PhotoVtg1000w,
+      width1200: PhotoVtg1200w,
+      width1600: PhotoVtg1600w,
+      width2000: PhotoVtg2000w,
+    },
     description: 'The website of a local car dealer in Germany.',
     descriptionLong:
       'VTG-Musli is a local car dealer in northern Germany. The goal was to create a simple and clean website that showcases the services and partners of the company. ',
@@ -43,7 +106,15 @@ const PROJECTS = [
   {
     id: '3',
     title: 'Portfolio-V1',
-    image: PhotoPortfolioV1,
+    image: {
+      width400: PhotoPortfolioV1400w,
+      width600: PhotoPortfolioV1600w,
+      width800: PhotoPortfolioV1800w,
+      width1000: PhotoPortfolioV11000w,
+      width1200: PhotoPortfolioV11200w,
+      width1600: PhotoPortfolioV11600w,
+      width2000: PhotoPortfolioV12000w,
+    },
     description: 'The first version of my portfolio website.',
     descriptionLong:
       'This is the first version of my personal portfolio website. I focused on creating a clean and simple portfolio that showcases my projects and skills. ',
@@ -59,7 +130,15 @@ const PROJECTS = [
   {
     id: '4',
     title: 'Flashcards',
-    image: PhotoFlashcards,
+    image: {
+      width400: PhotoFlashcards400w,
+      width600: PhotoFlashcards600w,
+      width800: PhotoFlashcards800w,
+      width1000: PhotoFlashcards1000w,
+      width1200: PhotoFlashcards1200w,
+      width1600: PhotoFlashcards1600w,
+      width2000: PhotoFlashcards2000w,
+    },
     description:
       'Flashcards is a web application where you can create flashcards and keep track of your learning progress.',
     descriptionLong:
@@ -88,7 +167,15 @@ const PROJECTS = [
   {
     id: '5',
     title: 'Music Prototype',
-    image: PhotoMusic,
+    image: {
+      width400: PhotoMusic400w,
+      width600: PhotoMusic600w,
+      width800: PhotoMusic800w,
+      width1000: PhotoMusic1000w,
+      width1200: PhotoMusic1200w,
+      width1600: PhotoMusic1600w,
+      width2000: PhotoMusic2000w,
+    },
     description: 'The Prototype of a music player web application.',
     descriptionLong:
       "This is the project that I built during the course 'Vue - The complete guide by Zero to Mastery'. It is a music player web application with user authentication. Users can upload their own songs and comment on other songs.",
@@ -105,7 +192,15 @@ const PROJECTS = [
   {
     id: '6',
     title: 'Portfolio-Advanced',
-    image: PhotoPortfolioAdvanced,
+    image: {
+      width400: PhotoPortfolioAdvanced400w,
+      width600: PhotoPortfolioAdvanced600w,
+      width800: PhotoPortfolioAdvanced800w,
+      width1000: PhotoPortfolioAdvanced1000w,
+      width1200: PhotoPortfolioAdvanced1200w,
+      width1600: PhotoPortfolioAdvanced1600w,
+      width2000: PhotoPortfolioAdvanced2000w,
+    },
     description:
       'The second Version of my portfolio website. Implemented netlify functions to create costum website analytics in MongoDB.',
     descriptionLong:
