9d18a7bc1e386b11b13b839c2cb7ea5dcd2f277c
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-08-26 15:19:32 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marco-developer-portfolio.netlify.app',
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind(), icon(), sitemap()],
   devToolbar: {
-    enabled: false,
-  },
+    enabled: false
+  }
 });
