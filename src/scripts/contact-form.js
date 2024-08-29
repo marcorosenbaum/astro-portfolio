f1e4898472e2b35e93d9a402cff5dc1b39cb9051
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-08-29 16:49:52 +0200

    chore: add contact form modal

---
+++ src/scripts/contact-form.js
@@ -0,0 +1,31 @@
+const showContactFormButton = document.querySelector(
+  '#show-contact-form-button'
+);
+if (showContactFormButton) {
+  showContactFormButton.addEventListener('click', () => {
+    const contactFormModal = document.querySelector('#contact-form-modal');
+    if (contactFormModal) {
+      contactFormModal.classList.remove('translate-x-[100%]');
+    }
+    const body = document.querySelector('body');
+    if (body) {
+      body.classList.add('overflow-hidden');
+    }
+  });
+}
+
+const hideContactFormButton = document.querySelector(
+  '#hide-contact-form-button'
+);
+if (hideContactFormButton) {
+  hideContactFormButton.addEventListener('click', () => {
+    const contactFormModal = document.querySelector('#contact-form-modal');
+    if (contactFormModal) {
+      contactFormModal.classList.add('translate-x-[100%]');
+    }
+    const body = document.querySelector('body');
+    if (body) {
+      body.classList.remove('overflow-hidden');
+    }
+  });
+}
