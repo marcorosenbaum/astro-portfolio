3335a8e4b6ff876f4155df23855f805246aa83b3
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-09-12 16:40:38 +0200

    add enable darkMode in tailwind config and restyle buttons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        "accent-color": "#04fbd6",
-        "accent-color-dark": "#05e0bf",
-        "custom-dark": "rgb(29 42 62)",
+        'accent-color': '#04fbd6',
+        'accent-color-dark': '#05e0bf',
+        'custom-dark': 'rgb(29 42 62)',
       },
       backgroundImage: {
-        "custom-gradient":
-          "linear-gradient(299deg, rgba(11,14,19,1) 0%, rgba(58,77,105,1) 48%, rgba(11,14,19,1) 100%)", // Add custom gradient
+        'custom-gradient':
+          'linear-gradient(299deg, rgba(11,14,19,1) 0%, rgba(58,77,105,1) 48%, rgba(11,14,19,1) 100%)', // Add custom gradient
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
