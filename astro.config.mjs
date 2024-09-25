a7b85add92ad32d2908815c9217790a164b9fedc
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-09-25 17:09:59 +0200

    fix: add show-contact-form-btn class to button

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,25 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://marco-developer-portfolio.netlify.app',
-  integrations: [react(), tailwind(), icon(), sitemap()],
+  site: 'https://marcorosenbaum.com',
+  integrations: [
+    react(),
+    tailwind(),
+    icon(),
+    sitemap({
+      filter: (page) => !page.includes('/exclude-this-page'),
+      serialize: (page) => {
+        return {
+          url: page,
+          lastmod: new Date().toISOString(),
+        };
+      },
+    }),
+  ],
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
